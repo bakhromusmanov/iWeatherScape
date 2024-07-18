438e00eb21f19953dc83b0a25c7ce1f18d2517ac
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-07-18 17:36:35 +0500

    Initial Commit

---
+++ iWeatherScape/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iWeatherScape
+//
+//  Created by Bakhrom Usmanov on 18/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
