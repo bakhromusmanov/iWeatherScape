c607879ec47cf5093408c01c2e8988db9c178056
Bakhrom Usmanov <bakhromusmanov01@gmail.com>
2024-07-21 13:36:13 +0500

    Refactor ViewController logic to WeatherBrain to keep MVC architecture design pattern

--- iWeatherScape/Controller/ViewController.swift
+++ iWeatherScape/Controller/ViewController.swift
@@ -11,16 +11,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var selectedCell: WeatherCell?
-    
-    let weather: [Weather] = [
-        Weather(image: UIImage(systemName: K.sunImageTitle)!, title: NSLocalizedString("sun", comment: "Weather condition: sunny")),
-        Weather(image: UIImage(systemName: K.rainImageTitle)!, title: NSLocalizedString("rain", comment: "Weather condition: rainy")),
-        Weather(image: UIImage(systemName: K.thunderstormImageTitle)!, title: NSLocalizedString("thunderstorm", comment: "Weather condition: thunderstorm")),
-        Weather(image: UIImage(systemName: K.fogImageTitle)!, title: NSLocalizedString("fog", comment: "Weather condition: foggy")),
-        Weather(image: UIImage(systemName: K.snowImageTitle)!, title: NSLocalizedString("snow", comment: "Weather condition: snowy")),
-        Weather(image: UIImage(systemName: K.cloudyImageTitle)!, title: NSLocalizedString("cloudy", comment: "Weather condition: cloudy"))
-    ]
+    let weatherBrain = WeatherBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,83 +26,34 @@ class ViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        loadAllCells()
-        selectRandomCell()
-    }
-    
-    
-    func loadAllCells() {
-        let indices = 0..<weather.count
-        let randomIndex = indices.randomElement()!
-        let indexPath = IndexPath(item: randomIndex, section: 0)
-        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: false)
-        collectionView.layoutIfNeeded()
-        collectionView.reloadItems(at: indices.map { IndexPath(item: $0, section: 0) })
-        if let cell = collectionView.cellForItem(at: indexPath) as? WeatherCell {
-            selectCell(cell)
-        }
-    }
-    
-    func selectRandomCell() {
-        let randomIndexPath = IndexPath(item: Int.random(in: 0..<weather.count), section: 0)
-        if let cell = collectionView.cellForItem(at: randomIndexPath) as? WeatherCell {
-            selectCell(cell)
-        }
-    }
-    
-    func selectCell(_ cell: WeatherCell) {
-        // Deselect the previously selected cell
-        if let selectedCell = self.selectedCell {
-            deselectCell(selectedCell)
-        }
-        
-        // Animate the selection
-        UIView.animate(withDuration: 0.3) {
-            cell.contentView.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
-            cell.titleLabel.alpha = 0.8
-            cell.imageView.tintColor = cell.changeColor()?.withAlphaComponent(0.6)
-            cell.backgroundColor = .systemGray5
-        }
-        
-        //Set cell as selected
-        selectedCell = cell
-    }
-    
-    func deselectCell(_ cell: WeatherCell) {
-        UIView.animate(withDuration: 0.3) {
-            cell.contentView.transform = .identity
-            cell.titleLabel.alpha = 1.0
-            cell.imageView.tintColor = cell.changeColor()
-            cell.backgroundColor = .clear
-        }
+        weatherBrain.selectRandomCell(collectionView)
     }
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate{
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return weather.count
+        return weatherBrain.weatherData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: K.cellIdentifier, for: indexPath) as! WeatherCell
-        cell.titleLabel.text = weather[indexPath.row].title
-        cell.imageView.image = weather[indexPath.row].image
+        cell.titleLabel.text = weatherBrain.weatherData[indexPath.row].title
+        cell.imageView.image = weatherBrain.weatherData[indexPath.row].image
         cell.imageView.tintColor = cell.changeColor()
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if let cell = collectionView.cellForItem(at: indexPath) as? WeatherCell {
-            selectCell(cell)
-            
+            weatherBrain.selectCell(cell)
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         if let cell = collectionView.cellForItem(at: indexPath) as? WeatherCell {
             // Animate the deselection
-            deselectCell(cell)
+            weatherBrain.deselectCell(cell)
         }
     }
 }
--- iWeatherScape/Model/WeatherBrain.swift
+++ iWeatherScape/Model/WeatherBrain.swift
@@ -5,4 +5,61 @@
 //  Created by Bakhrom Usmanov on 21/07/24.
 //
 
-import Foundation
+import UIKit
+
+class WeatherBrain {
+    var weatherData: [Weather] = []
+    var selectedCell: WeatherCell?
+
+    init() {
+        weatherData = [
+            Weather(image: UIImage(systemName: K.sunImageTitle)!, title: NSLocalizedString("sun", comment: "Weather condition: sunny")),
+            Weather(image: UIImage(systemName: K.rainImageTitle)!, title: NSLocalizedString("rain", comment: "Weather condition: rainy")),
+            Weather(image: UIImage(systemName: K.thunderstormImageTitle)!, title: NSLocalizedString("thunderstorm", comment: "Weather condition: thunderstorm")),
+            Weather(image: UIImage(systemName: K.fogImageTitle)!, title: NSLocalizedString("fog", comment: "Weather condition: foggy")),
+            Weather(image: UIImage(systemName: K.snowImageTitle)!, title: NSLocalizedString("snow", comment: "Weather condition: snowy")),
+            Weather(image: UIImage(systemName: K.cloudyImageTitle)!, title: NSLocalizedString("cloudy", comment: "Weather condition: cloudy"))
+        ]
+    }
+
+    func selectRandomCell(_ collectionView: UICollectionView) {
+        let indices = 0..<weatherData.count
+        let randomIndex = indices.randomElement()!
+        let indexPath = IndexPath(item: randomIndex, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: false)
+        collectionView.layoutIfNeeded()
+        collectionView.reloadItems(at: indices.map { IndexPath(item: $0, section: 0) })
+        if let cell = collectionView.cellForItem(at: indexPath) as? WeatherCell {
+            selectCell(cell)
+        }
+    }
+    
+    func selectCell(_ cell: WeatherCell) {
+        checkCellSelection(cell)
+        // Animate the selection
+        UIView.animate(withDuration: 0.3) {
+            cell.contentView.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            cell.titleLabel.alpha = 0.8
+            cell.imageView.tintColor = cell.changeColor()?.withAlphaComponent(0.6)
+            cell.backgroundColor = .systemGray5
+        }
+    }
+    
+    func deselectCell(_ cell: WeatherCell) {
+        UIView.animate(withDuration: 0.3) {
+            cell.contentView.transform = .identity
+            cell.titleLabel.alpha = 1.0
+            cell.imageView.tintColor = cell.changeColor()
+            cell.backgroundColor = .clear
+        }
+    }
+    
+    func checkCellSelection(_ cell: WeatherCell){
+        // Deselect the previously selected cell
+        if let selectedCell = self.selectedCell {
+            deselectCell(selectedCell)
+        }
+        //Set cell as selected
+        selectedCell = cell
+    }
+}
